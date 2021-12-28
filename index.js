7466f34a85d2fe728f170eb071b95027bebece0b
gotodoweb <gotodo@internet.ru>
2021-12-28 22:22:16 +0500

    ML2L3

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const title = document.querySelector('.modal__title');
+
+const form = document.querySelector('.modal__form');
+
+const checkbox = document.querySelector('.modal__fieldset');
+
+const checkboxfooter = document.querySelector('.modal__footer');
